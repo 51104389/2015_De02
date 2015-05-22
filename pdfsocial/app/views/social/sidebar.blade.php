c627c178a6481bee4a95ddbdb59128d38f116f01
51104389 <xuaniii113@gmail.com>
2015-05-23 02:27:37 +0700

    khong sai toi

--- pdfsocial/app/views/social/sidebar.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="col-sm-3">
-    <div class="list-group my-sidebar">
-        @foreach($allCats as $cat)
-        <a href="/social/cat?id={{ $cat->id }}" class="list-group-item list-group-item-danger">
-            {{ $cat->title }}
-        </a>
-        @endforeach
-    </div>
-</div>
